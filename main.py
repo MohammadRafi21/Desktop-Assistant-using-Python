8f7356bff3e6c9835acba68d1bc6c16c36ec65ab
G. M. Rafi <77846604+MohammadRafi21@users.noreply.github.com>
2026-01-04 04:05:56 +1100

    updated

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia 
+import webbrowser
+import os
+
+
+# Taking voice from my system
+engine = pyttsx3.init('sapi5') # to access the voice property in the system we have to use 'sapi5'
+voices = engine.getProperty('voices')
+rate = engine.getProperty('rate')
+#print(voices[0].id)
+#print(voices[1].id)
+engine.setProperty('voice',voices[0].id)
+engine.setProperty('rate', 150)
+
+# speak function
+
+def speak(text):
+    """
+    Docstring for speak
+    it takes text and return voice
+    
+    :param text: the text take string data type
+    """
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello I am a programmer, How are you?")
+
+# 
