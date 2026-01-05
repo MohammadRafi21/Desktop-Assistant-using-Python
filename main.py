e6e28526660a81c2e46235d057696055857e2354
G. M. Rafi <77846604+MohammadRafi21@users.noreply.github.com>
2026-01-05 18:16:14 +1100

    updated

--- main.py
+++ main.py
@@ -51,5 +51,33 @@ def takeCommand():
         return query
     
 #takeCommand()
-text = takeCommand()
-speak(text)
+# text = takeCommand()
+# speak(text)
+
+
+if __name__ == "__main__":
+
+    query = takeCommand().lower()
+
+
+    if "wikipedia" in query:
+        speak("Searching wikipedia")
+        query = query.replace('wikipedia','')
+        results = wikipedia.summary(query, sentences = 2)
+        speak("According to wikipedia")
+        print(results)
+        speak(results)
+    
+    elif "youtube" in query:
+        speak("Opening Youtube")
+        webbrowser.open("youtube.com")
+    
+    elif "google" in query:
+        speak("Opening google")
+        webbrowser.open("google.com")
+
+    elif "github" in query:
+        speak("Opening github")
+        webbrowser.open("github.com")
+
+    
